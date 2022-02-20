a40fc16c73898f28f573ad3ff9b3177ecf8c1de0
JhonatanAragao <ajhonatamslash@gmail.com>
2022-02-20 16:08:18 -0300

    atualizado

--- Ex_conversor_de_temperaturas.java
+++ Ex_conversor_de_temperaturas.java
@@ -1,3 +1,12 @@
+// Faça um algoritimo que converte uma dada temperatura, fornecida pelo usuário entre as escalas Celsius(C), Fahrenheit(F) e Kelvin(K)
+// 1 - Se o usuário fornecer a temperatura em °C  converta para °F e °K.
+// 2 - Se o usuário fornecer a temperatura em °F  converta para °C e °K.
+// 3 - Se o usuário fornecer a temperatura em °K  converta para °C e °F.
+
+//Aluno: Jhonatan Aragão Costa
+//Curso: Ciências da computação
+//Instituição: AESPI - FAPI
+
 import javax.swing.JOptionPane;
 
 public class Ex_conversor_de_temperaturas {
@@ -27,14 +36,17 @@ public class Ex_conversor_de_temperaturas {
                 System.out.print("Encerrando o programa...");
                 break;
             }
+
+            //**Tentativa de usar o método 'Try' sem sucesso!**
+
             //try {
-            //    int a_e = Integer.parseInt(JOptionPane.showInputDialog("Digite algo"));
+            //    int userChoice = Integer.parseInt(JOptionPane.showInputDialog("Digite algo"));
             //} catch (NumberFormatException userChoice) {
             //    System.out.println("Digite apenas números!");
             //} finally {
-             //   int a_e;
-              //  if (a_e == 1){
-              // System.out.println("1");
+             //   
+              //  if (userChoice == 1){
+              //    System.out.println("1");
                // }
             }
         }
